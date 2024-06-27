e1976a82a92ad396d880bc3dd9454031b5729641
Luca Anequini Antoniazzi <lucanequini@gmail.com>
2024-06-27 14:48:54 -0300

    feat: create home page and build its components

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,25 @@
+import getBillboard from "@/actions/get-billboard"
+import getProducts from "@/actions/get-products"
+
+import { Container } from "@/components/ui/container"
+import { Billboard } from "@/components/billboard"
+import { ProductList } from "@/components/product-list"
+
+export const revalidate = 0
+
+const HomePage = async () => {
+    const products = await getProducts({ isFeatured: true })
+    const billboard = await getBillboard('a60aa18f-6e3c-4cbb-9011-a3851a162050')
+    return (
+        <Container>
+            <div className="space-y-10 pb-10">
+                <Billboard data={billboard} />
+                <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                    <ProductList title='Featured Products' items={products} />
+                </div>
+            </div>
+        </Container>
+    )
+}
+
+export default HomePage
---
+++ components/ui/container.tsx
@@ -0,0 +1,13 @@
+interface ContainerProps {
+    children: React.ReactNode
+}
+
+export const Container: React.FC<ContainerProps> = ({
+    children
+}) => {
+    return (
+        <div className="mx-auto max-w-7xl">
+            {children}
+        </div>
+    )
+}
