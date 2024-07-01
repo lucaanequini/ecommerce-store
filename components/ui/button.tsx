31969edba4e484ff0d83c80fa126ea4494148a35
Luca Anequini Antoniazzi <lucanequini@gmail.com>
2024-06-30 23:51:43 -0300

    feat: create specif category page and start to build its components

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -17,6 +17,7 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
                 className
             )}
             ref={ref}
+            {...props}
         >
             {children}
         </button>
