ed1684921499e17e120247d592040cd35abf894c
Luca Anequini Antoniazzi <lucanequini@gmail.com>
2024-07-01 11:48:29 -0300

    feat: create expand functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 
 import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
+import { ModalProvider } from "@/providers/modal-provider";
 
 const font = Urbanist({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <Navbar />
+        <ModalProvider />
         {children}
         <Footer />
       </body>
--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { MouseEventHandler } from "react"
 import Image from "next/image"
 import { Expand, ShoppingCart } from "lucide-react"
 import { useRouter } from "next/navigation"
@@ -7,6 +8,7 @@ import { useRouter } from "next/navigation"
 import { Product } from "@/types"
 import { IconButton } from "@/components/ui/icon-button"
 import { Currency } from "@/components/ui/currency"
+import { usePreviewModal } from "@/hooks/use-preview-modal"
 
 interface ProductCardProps {
     data: Product
@@ -15,11 +17,16 @@ interface ProductCardProps {
 export const ProductCard: React.FC<ProductCardProps> = ({
     data
 }) => {
+    const previewModal = usePreviewModal()
     const router = useRouter()
     const handleClick = () => {
         router.push(`/product/${data?.id}`)
     }
 
+    const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+        event.stopPropagation()
+        previewModal.onOpen(data)
+    }
 
     return (
         <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
@@ -34,7 +41,7 @@ export const ProductCard: React.FC<ProductCardProps> = ({
                 <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
                     <div className="flex gap-x-6 justify-center">
                         <IconButton
-                            onClick={() => { }}
+                            onClick={onPreview}
                             icon={<Expand size={20} className="text-gray-600" />}
                         />
                         <IconButton
